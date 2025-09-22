36f38df2772fa024b202fb2f681f974f87462423
Mohit Dharmadhikari <mohit.dharma@gmail.com>
2025-09-22 23:53:11 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Index() {
   return (
     <div>
-      <Image src="/logo.png" alt="iximiuz Labs logo" width={22} height={22} />
+      <Image src="/logo.png" alt="VoidInfinity logo" width={22} height={22} />
       &nbsp;
       Welcome to VoidInfinity!
     </div>
